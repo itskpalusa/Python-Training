eb96d5a4f147b13c186a920e5d4b7e042b291da3
Karthik Palusa <karthik.palusa@gmail.com>
2017-12-22 17:44:38 -0700

    Added a Search Function
    Added a Search and Clear Function
    
    Fixed some run bugs, changed database

--- Databases/diary.py
+++ Databases/diary.py
@@ -2,15 +2,16 @@
 
 from collections import OrderedDict
 import datetime
+import os
 import sys
 
 from peewee import *
 
-db = SqliteDatabase('diary.db')
+db = SqliteDatabase('diaryentries.db')
 
 
 class Entry(Model):
-    context = TextField()
+    content = TextField()
     timestamp = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
@@ -48,18 +49,43 @@ def add_entry():
             Entry.create(content=data)
             print("Saved Successfully!")
 
-
-def view_entries():
+def view_entries(search_query=None):
     """View previous entries."""
+    entries = Entry.select().order_by(Entry.timestamp.desc())
+    if search_query:
+        entries = entries.where(Entry.content.contains(search_query))
 
+    for entry in entries:
+        timestamp = entry.timestamp.strftime('%A %B %d, %Y %I:%M%p')
+        print(timestamp)
+        print('='*len(timestamp))
+        print(entry.content)
+        print('n) next entry')
+        print('d) delete entry')
+        print('q) return to main menu')
+
+        next_action = input('Action: [Ndq] ').lower().strip()
+        if next_action == 'q':
+            break
+        elif next_action == 'd':
+            delete_entry(entry)
+            print("entry deleted")
+
+            
+def search_entries():
+    """Search entries for a string."""
+    view_entries(input('Search query: '))
 
 def delete_entry(entry):
     """Delete an entry."""
+    if input("Are you sure [yN] ").lower() == 'y':
+        entry.delete_instance()
 
 
 menu = OrderedDict([
     ('a', add_entry),
     ('v', view_entries),
+    ('s', search_entries),
 ])
 
 if __name__ == '__main__':
